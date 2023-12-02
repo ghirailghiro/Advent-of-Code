921021902ade7340900c2fb72d92a6f106d5c587
Michele Ghirardelli <50736672+ghirailghiro@users.noreply.github.com>
2023-12-02 13:18:43 +0100

    Create main.py

---
+++ python/day_2/main.py
@@ -0,0 +1,48 @@
+lines = []  
+with open("input.txt") as my_file:
+    lines = my_file.readlines()
+
+red_limit = 12
+green_limit = 13
+blue_limit = 14
+
+def doLine(line):
+    line = line.replace('\n', '')
+    runs = line.split('; ')
+    runs_and_game = runs[0].split(': ')
+    runs[0] = runs_and_game[1]
+    runs = {"GameNumber": int(runs_and_game[0].split(" ")[1]), "Runs": runs}
+    return runs
+
+def doRun(run):
+    runSplitted = run.split(', ')
+    return runSplitted
+
+def isRunValid(value, limit):
+    return value <= limit
+
+countValidRuns = 0
+
+for line in lines:
+    flagValid = True
+    runs = doLine(line)
+    for run in runs["Runs"]:
+        runSplitted = doRun(run)
+        for step in runSplitted:
+            singleStep = step.split(' ')
+            if singleStep[1] == 'red':
+                if (not isRunValid(int(singleStep[0]), red_limit)):
+                    flagValid = False
+            elif singleStep[1] == 'green':
+                if (not isRunValid(int(singleStep[0]), green_limit)):
+                    flagValid = False
+            elif singleStep[1] == 'blue':
+                if (not isRunValid(int(singleStep[0]), blue_limit)):
+                    flagValid = False
+            else:
+                #throw exception and stop the program
+                print('Invalid color')
+    if flagValid:
+        countValidRuns += runs['GameNumber']
+
+print(f"Part 1 Value: {countValidRuns}")
