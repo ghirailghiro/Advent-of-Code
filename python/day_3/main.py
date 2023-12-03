36533128d56cb4036bbbb0f1822ae758d6c0da96
ghirailghiro <50736672+ghirailghiro@users.noreply.github.com>
2023-12-03 13:38:35 +0100

    first part done

--- python/day_3/main.py
+++ python/day_3/main.py
@@ -1,25 +1,73 @@
 lines = []  
-with open("test_input.txt") as my_file:
+with open("input.txt") as my_file:
     lines = my_file.readlines()
 
 sumAux = 0
+possible_simbols = ["!", "@", "#", "$", "%","^","&","*","(",")","-","+","?","_","=",",","<",">","/"]
 
 for i in range(len(lines)):
     aux = ""
     firstIndex = None
     lastIndex = None
     for j in range(len(lines[i])):
-        breakpoint()
         if(lines[i][j].isnumeric()):
             aux += lines[i][j]
             if firstIndex is None:
                 firstIndex = j
             lastIndex = j
         elif(firstIndex is not None and lastIndex is not None):
-            breakpoint()
+            preLine = ""
+            postLine = ""
+            if(firstIndex!=0):
+                firstIndex -= 1
+            if(lastIndex!=len(lines[i])-1):
+                lastIndex +=2
+            else:
+                lastIndex+=1
+            if(i!=0):
+                preLine = lines[i-1][firstIndex:lastIndex]
+            if(i!=len(lines)-1):
+                postLine = lines[i+1][firstIndex:lastIndex] 
+            checkLine = preLine + postLine + lines[i][firstIndex:lastIndex]
+            if  any([k in checkLine for k in possible_simbols]):
+                print(aux)
+                sumAux += int(aux)
+            aux = ""
             firstIndex = None
             lastIndex= None
-            if "*" in lines[i-1:i+1][j-len(aux):j+len(aux)]:
-                sumAux += int(aux)
                 
+print(f"Part 1 Value: {sumAux}")
+
+
+for i in range(len(lines)):
+    aux = ""
+    firstIndex = None
+    lastIndex = None
+    for j in range(len(lines[i])):
+        if(lines[i][j] == "*"):
+            aux += lines[i][j]
+            if firstIndex is None:
+                firstIndex = j
+            lastIndex = j
+            preLine = ""
+            postLine = ""
+            if(firstIndex!=0):
+                firstIndex -= 1
+            if(lastIndex!=len(lines[i])-1):
+                lastIndex +=2
+            else:
+                lastIndex+=1
+            if(i!=0):
+                preLine = lines[i-1][firstIndex:lastIndex]
+            if(i!=len(lines)-1):
+                postLine = lines[i+1][firstIndex:lastIndex] 
+            checkLine = preLine + postLine + lines[i][firstIndex:lastIndex]
+            if  any([k in checkLine for k in possible_simbols]):
+                print(aux)
+                sumAux += int(aux)
+            aux = ""
+            firstIndex = None
+            lastIndex= None
+                
+print(f"Part 2 Value: {sumAux}")
 
