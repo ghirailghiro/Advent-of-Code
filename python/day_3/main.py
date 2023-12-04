b968573c59426af408dc4f9d307f6454d1cbbffb
ghirailghiro <50736672+ghirailghiro@users.noreply.github.com>
2023-12-04 15:38:36 +0100

    day 3 completed

--- python/day_3/main.py
+++ python/day_3/main.py
@@ -1,10 +1,10 @@
 lines = []  
-with open("test_input.txt") as my_file:
+with open("input.txt") as my_file:
     lines = my_file.readlines()
 
 sumAux = 0
 possible_simbols = ["!", "@", "#", "$", "%","^","&","*","(",")","-","+","?","_","=",",","<",">","/"]
-'''
+
 for i in range(len(lines)):
     aux = ""
     firstIndex = None
@@ -30,59 +30,53 @@ for i in range(len(lines)):
                 postLine = lines[i+1][firstIndex:lastIndex] 
             checkLine = preLine + postLine + lines[i][firstIndex:lastIndex]
             if  any([k in checkLine for k in possible_simbols]):
-                print(aux)
                 sumAux += int(aux)
             aux = ""
             firstIndex = None
             lastIndex= None
                 
 print(f"Part 1 Value: {sumAux}")
-'''
 
 
+def checkNumbersNearSymbol(line,firstIndex,lastIndex):
+    list_of_adjagent_num = []
+    auxIntToInsertPost=""
+    for k in range(len(line)):
+        if(line[k].isdigit()):
+            auxIntToInsertPost+=line[k]
+        elif(auxIntToInsertPost!= ""):
+            if(k>firstIndex and k-len(auxIntToInsertPost) <= lastIndex):
+                list_of_adjagent_num.append(int(auxIntToInsertPost))
+            auxIntToInsertPost = ""
+    return list_of_adjagent_num
 
+sumAux = 0
 for i in range(len(lines)):
     aux = ""
     for j in range(len(lines[i])):
         if(lines[i][j] == "*"):
             firstIndex = j
             lastIndex = j
-            breakpoint()
             #aux += lines[i][j]
             preLine = ""
             postLine = ""
             if(j!=0):
                 firstIndex -= 1
             if(j!=len(lines[i])-1):
-                lastIndex +=2
-            else:
                 lastIndex +=1
             if(i!=0):
-                preLine = lines[i-1][firstIndex:lastIndex]
+                preLine = lines[i-1][firstIndex:lastIndex+1]
             if(i!=len(lines)-1):
-                postLine = lines[i+1][firstIndex:lastIndex] 
+                postLine = lines[i+1][firstIndex:lastIndex+1] 
             list_of_adjagent_num = []
-            auxIntToInsertPost = ""
-            auxIntToInsertPre = ""
             if(preLine != ""):
-                for k in range(len(lines[i-1])):
-                    if(preLine != "" and lines[i-1][k].isdigit()):
-                        auxIntToInsertPre+=lines[i-1][k]
-                    elif(preLine != "" and auxIntToInsertPre!= ""):
-                        if(k+len(auxIntToInsertPre)>=lastIndex & k-len(auxIntToInsertPre) <=firstIndex):
-                            breakpoint()
-                            list_of_adjagent_num.insert(int(auxIntToInsertPre))
-                        auxIntToInsertPre = ""    
+                list_of_adjagent_num += checkNumbersNearSymbol(lines[i-1],firstIndex,lastIndex)   
             if(postLine != ""):
-                for k in range(len(lines[i+1])):
-                    if(postLine != "" and lines[i+1][k].isdigit()):
-                        auxIntToInsertPost+=lines[i+1][k]
-                    elif(postLine != "" and auxIntToInsertPost!= ""):
-                        if(k+len(auxIntToInsertPost)>=lastIndex & k-len(auxIntToInsertPost) <=firstIndex):
-                            breakpoint()
-                            list_of_adjagent_num.insert(int(auxIntToInsertPost))
-                        auxIntToInsertPost = ""
-
+                list_of_adjagent_num += checkNumbersNearSymbol(lines[i+1],firstIndex,lastIndex) 
+            list_of_adjagent_num += checkNumbersNearSymbol(lines[i],firstIndex,lastIndex) 
+            #breakpoint()
+            if(len(list_of_adjagent_num) == 2):
+                sumAux += list_of_adjagent_num[0]*list_of_adjagent_num[1]
                     
                                 
             #checkLine = preLine + postLine + lines[i][firstIndex:lastIndex]
